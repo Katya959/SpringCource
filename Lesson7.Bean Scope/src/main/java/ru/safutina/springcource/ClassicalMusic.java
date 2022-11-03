4bf90344580a9f548a1dc83b2468ca8e8b342355
Katya959 <61267054+Katya959@users.noreply.github.com>
2022-11-03 20:09:08 +0300

    Add scope of bean

---
+++ Lesson7.Bean Scope/src/main/java/ru/safutina/springcource/ClassicalMusic.java
@@ -0,0 +1,8 @@
+package ru.safutina.springcource;
+
+public class ClassicalMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Hungarian Rhapsody";
+    }
+}
---
+++ Lesson7.Bean Scope/src/main/java/ru/safutina/springcource/TestSpring.java
@@ -0,0 +1,30 @@
+package ru.safutina.springcource;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        /*Сравнение по цказателям. Позволяет понять, указывают ли обе переменные на один и тот же участок памяти,
+        * на один и тот же объект. Equals сравнивает объекты по содержанию. */
+        boolean comparison = firstMusicPlayer == secondMusicPlayer;   // true - указатели указывают на один объект.
+
+        System.out.println(comparison);
+
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
+
+        firstMusicPlayer.setVolume(10);
+
+        System.out.println(firstMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());
+
+        context.close();
+    }
+}
