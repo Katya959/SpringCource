8b5b2b5f1d126a0ad3ab43af86da84a6b6bc21f0
Katya959 <61267054+Katya959@users.noreply.github.com>
2022-11-04 13:24:50 +0300

    Add annotation @Component

---
+++ Lesson9.Annotations.Introduction/src/main/java/ru/safutina/springcource/Music.java
@@ -0,0 +1,6 @@
+package ru.safutina.springcource;
+
+public interface Music {
+    //Возвращает одну песню в определенном жанре (в зав-ти от класса, объект которого создан).
+    String getSong();
+}
