a6a22072f0ac0ea38311b5eae6608b40427ae1bc
Katya959 <61267054+Katya959@users.noreply.github.com>
2022-11-05 00:54:55 +0300

    Add annotations PostConstruct and PreDestroy

---
+++ Lesson12.Annotations PostConstruct.PreDestroy/src/main/java/ru/safutina/springcource/TestSpring.java
@@ -0,0 +1,21 @@
+
+package ru.safutina.springcource;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
+
+        ClassicalMusic classicalMusic1 = context.getBean("classicalMusic", ClassicalMusic.class);
+
+        context.close();
+    }
+}
