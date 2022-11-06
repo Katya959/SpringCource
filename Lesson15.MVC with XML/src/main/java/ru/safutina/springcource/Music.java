aa55b21e3288037c93e378d9c1bd8ff2dde68df0
Katya959 <61267054+Katya959@users.noreply.github.com>
2022-11-06 21:48:47 +0300

    Delete Music.java

--- Lesson15.MVC with XML/src/main/java/ru/safutina/springcource/Music.java
+++
@@ -1,6 +0,0 @@
-package ru.safutina.springcource;
-
-public interface Music {
-    //Возвращает одну песню в определенном жанре (в зав-ти от класса, объект которого создан).
-    String getSong();
-}
